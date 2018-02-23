c9c35dbfe4fdf77bd70839f32cef8212a4d41164
Zachary Weeden <zdw7287@rit.edu>
2018-02-23 13:59:49 -0500

    Parsed for kernproc and rootvnode in stdout

--- offset.py
+++ offset.py
@@ -7,11 +7,13 @@ import subprocess
 
 firmwares = {}
 firmwares_url = {}  # {device: {size: link}, ...}
+device_offset_dict = {}
 ota_json = requests.request('GET', 'https://api.ipsw.me/v2.1/ota.json').json()
 for device in ota_json.keys():
     min_size = 10000000  # 10 MB - filter out ill-structure ipas (1.3 KB)
     max_size = 10000000000  # 10 GB
     if 'iPhone' in device:  # only iphones
+        device_offset_dict[device] = {}
         firmwares[device] = ota_json[device]['firmwares']
         for version in firmwares[device]:
             if version['size'] < max_size and version['size'] > min_size:
@@ -29,14 +31,19 @@ for device in ota_json.keys():
         # remove zip here
         kernelcache_location = f'{os.getcwd()}/{filename.split(".")[0]}/AssetData/boot/'
         kernelcache_location += \
-        [string for string in os.listdir(kernelcache_location) if
-         'kernelcache.release.' in string][0]
-        result = subprocess.run(['joker', '-dec', f'{kernelcache_location}'],
-                                stdout=subprocess.PIPE)
-        print(str(result.stdout.decode('utf8')))
+            [string for string in os.listdir(kernelcache_location) if
+             'kernelcache.release.' in string][0]
+        joker_decompression = subprocess.run(
+            ['joker', '-dec', f'{kernelcache_location}'],
+            stdout=subprocess.PIPE)
+        joker_results = joker_decompression.stdout
         jtool_symbol = subprocess.run(['jtool', '-S', '/tmp/kernel'],
                                       stdout=subprocess.PIPE)
-        print(str(jtool_symbol.stdout.decode('utf8')))
-        kernproc_offset = '_kernproc'
-        rootvnode_offset = '_rootvnode'
+        jtool_results = jtool_symbol.stdout
+        # grep-esque
+        for line in jtool_results.decode('utf8').split('\n'):
+            if '_kernproc' in line or '_rootvnode' in line:
+                device_offset_dict[device].update(
+                    {f'{line.split()[1]} {line.split()[2]}': line.split()[0]})
+        print(device_offset_dict)
         input('')
