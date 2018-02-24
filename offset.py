fdc97b806acc6f0dce7b8c0b2f261ba57c7eab93
Zachary Weeden <zdw7287@rit.edu>
2018-02-24 01:28:19 -0500

    Different offsets for different fw versions - specifying in macro and ignoring older devices

--- offset.py
+++ offset.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 # ensure joker and jtool downloaded and in path - symlink if needed
 import os
+import json
 import requests
 import zipfile
 import subprocess
+import shutil
 
+ios_version = '11.1'
 firmwares = {}
 firmwares_url = {}  # {device: {size: link}, ...}
 device_offset_dict = {}
@@ -17,33 +20,44 @@ for device in ota_json.keys():
         firmwares[device] = ota_json[device]['firmwares']
         for version in firmwares[device]:
             if version['size'] < max_size and version['size'] > min_size:
-                max_size = version['size']
-                firmwares_url[device] = {version["version"]: version['url']}
-        fileurl = list(firmwares_url[device].values())[0]
-        filename = fileurl.split('/')[-1]
-        print(f'Downloading zipped ipa: {device} :: {filename}...')
-        with open(f'/{os.getcwd()}/{filename}', 'wb') as f:
-            f.write(requests.request('GET', fileurl).content)
-        print(f'Unzipping ipa: {device} :: {filename}...')
-        zip_ref = zipfile.ZipFile(f'{os.getcwd()}/{filename}', 'r')
-        zip_ref.extractall(f'{os.getcwd()}/{filename.split(".")[0]}')
-        zip_ref.close()
-        # remove zip here
-        kernelcache_location = f'{os.getcwd()}/{filename.split(".")[0]}/AssetData/boot/'
-        kernelcache_location += \
-            [string for string in os.listdir(kernelcache_location) if
-             'kernelcache.release.' in string][0]
-        joker_decompression = subprocess.run(
-            ['joker', '-dec', f'{kernelcache_location}'],
-            stdout=subprocess.PIPE)
-        joker_results = joker_decompression.stdout
-        jtool_symbol = subprocess.run(['jtool', '-S', '/tmp/kernel'],
-                                      stdout=subprocess.PIPE)
-        jtool_results = jtool_symbol.stdout
-        # grep-esque
-        for line in jtool_results.decode('utf8').split('\n'):
-            if '_kernproc' in line or '_rootvnode' in line:
-                device_offset_dict[device].update(
-                    {f'{line.split()[1]} {line.split()[2]}': line.split()[0]})
-        print(device_offset_dict)
-        input('')
+                if version["version"] == ios_version:
+                    max_size = version['size']
+                    downloaded_version = version['version']
+                    firmwares_url[device] = {version["version"]: version['url']}
+        try:  # maybe use found flag and conditional to continue loop
+            fileurl = list(firmwares_url[device].values())[0]
+            filename = fileurl.split('/')[-1]
+            print(f'Downloading zipped ipa: {device} :: {downloaded_version} :: {filename}...')
+            with open(f'/{os.getcwd()}/{filename}', 'wb') as f:
+                f.write(requests.request('GET', fileurl).content)
+            print(f'Unzipping ipa: {device} :: {filename}...')
+            zip_ref = zipfile.ZipFile(f'{os.getcwd()}/{filename}', 'r')
+            zip_ref.extractall(f'{os.getcwd()}/{filename.split(".")[0]}')
+            zip_ref.close()
+            kernelcache_location = f'{os.getcwd()}/{filename.split(".")[0]}/AssetData/boot/'
+            kernelcache_location += \
+                [string for string in os.listdir(kernelcache_location) if
+                 'kernelcache.release.' in string][0]
+            # handle errors: "I have no idea how to handle a file with a magic of..."
+            # "Not handling IMG3. 32-bit is passe', man"
+            joker_decompression = subprocess.run(
+                ['joker', '-dec', f'{kernelcache_location}'],
+                stdout=subprocess.PIPE)
+            joker_results = joker_decompression.stdout
+            jtool_symbol = subprocess.run(['jtool', '-S', '/tmp/kernel'],
+                                          stdout=subprocess.PIPE)
+            jtool_results = jtool_symbol.stdout
+            # grep-esque
+            for line in jtool_results.decode('utf8').split('\n'):
+                if '_kernproc' in line or '_rootvnode' in line:
+                    device_offset_dict[device].update(
+                        {f'{line.split()[1]} {line.split()[2]}': line.split()[0]})
+            print(json.dumps(device_offset_dict, indent=4))
+            os.remove(f'{os.getcwd()}/{filename}')  # remove zip
+            try:
+                os.remove('/tmp/kernel')  # remove /tmp/kernel
+            except:
+                pass
+            shutil.rmtree(f'{os.getcwd()}/{filename.split(".")[0]}')  # remove unzipped folder
+        except:
+            continue
